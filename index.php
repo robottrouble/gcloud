2e9463dc3a8f493d8069edefe51c705f753c44de
akaufm200 <adam_kaufman@cable.comcast.com>
2017-06-01 16:44:45 -0400

    set default location to /

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(is_array($_FILES) && sizeof($_FILES) > 0)
         $target_path = $upload_dir . basename( $_FILES['uploadedfile']['name']);
         if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path) && chmod($target_path, 0775)) {
                 if(gcode_to_svg($target_path))
-                        header("Location: /gview/");
+                        header("Location: /");
                 echo "The file ".  basename( $_FILES['uploadedfile']['name']) . " has been uploaded";
         } else{
                 echo "There was an error uploading the file, please try again!";
